413df2d690bde400f5f418c5369a2b832f4b2420
Douman <douman@gmx.se>
2018-06-22 23:05:53 +0300

    1.0.0: Refactoring into ETag struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,59 +1,204 @@
-//! ETag Trait implementation for various types.
+//! Simple EntityTag implementation.
 //!
-//! For file's Metadata it uses following format `<modified>-<size>`
-//!
-//! For other types it uses `<len>-<hash>` that relies on `std::hash::Hash` and `DefaultHasher` of HashMap.
-//!
-//! ## Usage
-//! ```rust
-//! extern crate etag;
-//!
-//! use etag::Etag;
-//!
-//! fn main() {
-//!     println!("ETag for string={}", "string".etag());
-//! }
-//! ```
-
 use std::fs;
+use std::fmt;
 use std::time;
+use std::error;
 use std::hash::{
     Hash,
     Hasher
 };
 use std::collections::hash_map::DefaultHasher;
 
-///Trait that provides calculation of ETag field for type.
-pub trait Etag {
-    ///Calculates ETag value.
-    fn etag(&self) -> String;
+/// An entity tag, defined in [RFC7232](https://tools.ietf.org/html/rfc7232#section-2.3)
+///
+/// The ETag HTTP response header is an identifier for a specific version of a resource. It allows
+/// caches to be more efficient, and saves bandwidth, as a web server does not need to send a full
+/// response if the content has not changed. On the other side, if the content has changed, etags
+/// are useful to help prevent simultaneous updates of a resource from overwriting each other
+/// ("mid-air collisions").
+///
+/// If the resource at a given URL changes, a new Etag value must be generated. Etags are therefore
+/// similar to fingerprints and might also be used for tracking purposes by some servers. A
+/// comparison of them allows to quickly determine whether two representations of a resource are the
+/// same, but they might also be set to persist indefinitely by a tracking server.
+///
+///
+/// # Format `W/"<etag_value>"`
+///
+/// - 'W/' (case-sensitive) indicates that a weak validator is used. Weak validators are easy to
+/// generate but are far less useful for comparisons. Strong validators are ideal for comparisons
+/// but can be very difficult to generate efficiently. Weak Etag values of two representations of
+/// the same resources might be semantically equivalent, but not byte-for-byte identical.
+///
+/// - "<etag_value>" Entity tags uniquely representing the requested resources. They are a string of ASCII
+/// characters placed between double quotes (Like "675af34563dc-tr34"). The method by which ETag
+/// values are generated is not specified. Oftentimes, a hash of the content, a hash of the last
+/// modification timestamp, or just a revision number is used. For example, MDN uses a hash of
+/// hexadecimal digits of the wiki content.
+///
+/// # Comparison
+/// To check if two entity tags are equivalent in an application always use the
+/// `strong_eq` or `weak_eq` methods based on the context of the Tag. Only use
+/// `==` to check if two tags are identical.
+///
+/// The example below shows the results for a set of entity-tag pairs and
+/// both the weak and strong comparison function results:
+///
+/// | `ETag 1`| `ETag 2`| Strong Comparison | Weak Comparison |
+/// |---------|---------|-------------------|-----------------|
+/// | `W/"1"` | `W/"1"` | no match          | match           |
+/// | `W/"1"` | `W/"2"` | no match          | no match        |
+/// | `W/"1"` | `"1"`   | no match          | match           |
+/// | `"1"`   | `"1"`   | match             | match           |
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct EntityTag {
+    /// Weakness indicator for the tag
+    pub weak: bool,
+    /// The opaque string in between the DQUOTEs
+    tag: String,
 }
 
-impl Etag for fs::Metadata {
-    ///Calculates ETag from Metadata in following format `<modified>-<size>`.
+impl EntityTag {
+    /// Constructs a new EntityTag.
     ///
-    ///Note that if modified is not available then it is omitted.
-    fn etag(&self) -> String {
-        match self.modified().map(|modified| modified.duration_since(time::UNIX_EPOCH).expect("Modified is earlier than time::UNIX_EPOCH!")) {
-            Ok(modified) => format!("{}.{}-{}", modified.as_secs(), modified.subsec_nanos(), self.len()),
-            _ => format!("{}", self.len())
+    /// As tag characters must be in ASCII assert
+    /// is included to check for it.
+    pub fn new(weak: bool, tag: String) -> Self {
+        assert!(tag.is_ascii());
+        EntityTag { weak, tag }
+    }
+
+    /// Constructs a new weak EntityTag.
+    pub fn weak(tag: String) -> Self {
+        Self::new(true, tag)
+    }
+
+    /// Constructs a new strong EntityTag.
+    pub fn strong(tag: String) -> Self {
+        Self::new(false, tag)
+    }
+
+    /// Creates weak EntityTag from file metadata using modified time and len.
+    pub fn from_file_meta(metadata: &fs::Metadata) -> Self {
+        let tag = match metadata.modified().map(|modified| modified.duration_since(time::UNIX_EPOCH).expect("Modified is earlier than time::UNIX_EPOCH!")) {
+            Ok(modified) => format!("{}.{}-{}", modified.as_secs(), modified.subsec_nanos(), metadata.len()),
+            _ => format!("{}", metadata.len())
+        };
+
+        Self {
+            weak: true,
+            tag
+        }
+    }
+
+    /// Creates strong EntityTag by hashing provided bytes.
+    pub fn from_hash(bytes: &[u8]) -> Self {
+        let mut hasher = DefaultHasher::default();
+        bytes.hash(&mut hasher);
+        let tag = format!("{}-{}", bytes.len(), hasher.finish());
+
+        Self {
+            weak: false,
+            tag
+        }
+    }
+
+    /// Get the tag.
+    pub fn tag(&self) -> &str {
+        self.tag.as_ref()
+    }
+
+    /// Set the tag.
+    pub fn set_tag(&mut self, tag: String) {
+        self.tag = tag
+    }
+
+    /// For strong comparison two entity-tags are equivalent if both are not
+    /// weak and their opaque-tags match character-by-character.
+    pub fn strong_eq(&self, other: &EntityTag) -> bool {
+        !self.weak && !other.weak && self.tag == other.tag
+    }
+
+    /// For weak comparison two entity-tags are equivalent if their
+    /// opaque-tags match character-by-character, regardless of either or
+    /// both being tagged as "weak".
+    pub fn weak_eq(&self, other: &EntityTag) -> bool {
+        self.tag == other.tag
+    }
+
+    /// The inverse of `EntityTag.strong_eq()`.
+    pub fn strong_ne(&self, other: &EntityTag) -> bool {
+        !self.strong_eq(other)
+    }
+
+    /// The inverse of `EntityTag.weak_eq()`.
+    pub fn weak_ne(&self, other: &EntityTag) -> bool {
+        !self.weak_eq(other)
+    }
+}
+
+impl fmt::Display for EntityTag {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self.weak {
+            true => write!(f, "W/\"{}\"", self.tag),
+            false => write!(f, "\"{}\"", self.tag),
         }
     }
 }
 
-macro_rules! impl_with_hasher
-{
-    ($($t:ty), +) => {
-        $(
-            impl Etag for $t {
-                fn etag(&self) -> String {
-                    let mut hasher = DefaultHasher::default();
-                    self.hash(&mut hasher);
-                    format!("{}-{}", self.len(), hasher.finish())
-                }
-            }
-        )+
-    };
+///Describes possible errors for EntityTag
+#[derive(Debug)]
+pub enum ParseError {
+    ///Format of EntityTag is invalid
+    InvalidFormat,
+    ///Tag contains non-ASCII characters
+    NotAscii
 }
 
-impl_with_hasher!(str, String, [u8]);
+impl fmt::Display for ParseError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            ParseError::InvalidFormat => write!(f, "EntityTag uses invalid format"),
+            ParseError::NotAscii => write!(f, "EntityTag uses non-ASCII characters")
+        }
+    }
+}
+
+impl error::Error for ParseError {
+    fn description(&self) -> &str {
+        match self {
+            ParseError::InvalidFormat => "EntityTag uses invalid format",
+            ParseError::NotAscii => "EntityTag uses non-ASCII characters"
+        }
+    }
+}
+
+impl std::str::FromStr for EntityTag {
+    type Err = ParseError;
+
+    fn from_str(text: &str) -> Result<EntityTag, ParseError> {
+        let len = text.len();
+        let slice = &text[..];
+
+        if !slice.ends_with('"') || len < 2 {
+            return Err(ParseError::InvalidFormat);
+        }
+
+        if slice.starts_with('"') {
+            let slice = &slice[1..len-1];
+            match slice.is_ascii() {
+                true => Ok(EntityTag::strong(slice.to_string())),
+                false => Err(ParseError::NotAscii)
+            }
+        } else if len >= 4 && slice.starts_with("W/\"") {
+            let slice = &slice[3..len-1];
+            match slice.is_ascii() {
+                true => Ok(EntityTag::weak(slice.to_string())),
+                false => Err(ParseError::NotAscii)
+            }
+        } else {
+            Err(ParseError::InvalidFormat)
+        }
+    }
+}
--- tests/etag.rs
+++ tests/etag.rs
@@ -1,31 +1,83 @@
 extern crate etag;
 
-use etag::Etag;
 use std::fs;
 use std::time;
 
-#[test]
-fn calc_string() {
-    let string = "ololo".to_string();
-    let result = string.etag();
+use etag::EntityTag;
 
-    assert!(result.starts_with("5"));
+#[test]
+fn test_from_file_meta() {
+    let file = fs::File::open("Cargo.toml").expect("To open Cargo.toml");
+    let metadata = file.metadata().expect("To get metadata");
+    let etag = EntityTag::from_file_meta(&metadata);
+
+    assert_eq!(etag.weak, true);
+    //Make sure we stick to format
+    match metadata.modified().map(|modified| modified.duration_since(time::UNIX_EPOCH).expect("Modified is earlier than time::UNIX_EPOCH!")) {
+        Ok(modified) => assert_eq!(format!("{}.{}-{}", modified.as_secs(), modified.subsec_nanos(), metadata.len()), etag.tag()),
+        _ => assert_eq!(format!("{}", metadata.len()), etag.tag())
+    }
 }
 
 #[test]
-fn calc_buff() {
-    let buff = [1, 2, 3, 4];
-    let result = buff.etag();
+fn test_cmp() {
+    const FIRST: &'static str = "FIRST";
+    const SECOND: &'static str = "SECOND";
 
-    assert!(result.starts_with("4"));
+    let etag1 = EntityTag::weak(FIRST.to_owned());
+    let etag2 = EntityTag::weak(FIRST.to_owned());
+    assert!(!etag1.strong_eq(&etag2));
+    assert!(etag1.weak_eq(&etag2));
+    assert!(etag1.strong_ne(&etag2));
+    assert!(!etag1.weak_ne(&etag2));
+
+    let etag1 = EntityTag::weak(FIRST.to_owned());
+    let etag2 = EntityTag::weak(SECOND.to_owned());
+    assert!(!etag1.strong_eq(&etag2));
+    assert!(!etag1.weak_eq(&etag2));
+    assert!(etag1.strong_ne(&etag2));
+    assert!(etag1.weak_ne(&etag2));
+
+    let etag1 = EntityTag::weak(FIRST.to_owned());
+    let etag2 = EntityTag::strong(FIRST.to_owned());
+    assert!(!etag1.strong_eq(&etag2));
+    assert!(etag1.weak_eq(&etag2));
+    assert!(etag1.strong_ne(&etag2));
+    assert!(!etag1.weak_ne(&etag2));
+
+    let etag1 = EntityTag::strong(FIRST.to_owned());
+    let etag2 = EntityTag::strong(FIRST.to_owned());
+    assert!(etag1.strong_eq(&etag2));
+    assert!(etag1.weak_eq(&etag2));
+    assert!(!etag1.strong_ne(&etag2));
+    assert!(!etag1.weak_ne(&etag2));
 }
 
 #[test]
-fn calc_metadata() {
-    let file = fs::File::open(file!()).expect("Couldn't open file!");
-    let metadata = file.metadata().expect("Couldn't get metadata!");
-    let modified = metadata.modified().unwrap().duration_since(time::UNIX_EPOCH).expect("Modified is earlier than time::UNIX_EPOCH!");
-    let result = metadata.etag();
+fn test_etag_fmt() {
+    assert_eq!(format!("{}", EntityTag::strong("foobar".to_owned())), "\"foobar\"");
+    assert_eq!(format!("{}", EntityTag::strong("".to_owned())), "\"\"");
+    assert_eq!(format!("{}", EntityTag::weak("weak-etag".to_owned())), "W/\"weak-etag\"");
+    assert_eq!(format!("{}", EntityTag::weak("\u{0065}".to_owned())), "W/\"\x65\"");
+    assert_eq!(format!("{}", EntityTag::weak("".to_owned())), "W/\"\"");
+}
 
-    assert_eq!(result, format!("{}.{}-{}", modified.as_secs(), modified.subsec_nanos(), metadata.len()));
+#[test]
+fn test_etag_parse_success() {
+    assert_eq!("\"foobar\"".parse::<EntityTag>().unwrap(), EntityTag::strong("foobar".to_owned()));
+    assert_eq!("\"\"".parse::<EntityTag>().unwrap(), EntityTag::strong("".to_owned()));
+    assert_eq!("W/\"weaktag\"".parse::<EntityTag>().unwrap(), EntityTag::weak("weaktag".to_owned()));
+    assert_eq!("W/\"\x65\x62\"".parse::<EntityTag>().unwrap(), EntityTag::weak("\x65\x62".to_owned()));
+    assert_eq!("W/\"\"".parse::<EntityTag>().unwrap(), EntityTag::weak("".to_owned()));
+}
+
+#[test]
+fn test_etag_parse_failures() {
+    assert!("W/\"ろり\"".parse::<EntityTag>().is_err());
+    assert!("no-dquotes".parse::<EntityTag>().is_err());
+    assert!("w/\"the-first-w-is-case-sensitive\"" .parse::<EntityTag>() .is_err());
+    assert!("".parse::<EntityTag>().is_err());
+    assert!("\"unmatched-dquotes1".parse::<EntityTag>().is_err());
+    assert!("unmatched-dquotes2\"".parse::<EntityTag>().is_err());
+    assert!("matched-\"dquotes\"".parse::<EntityTag>().is_err());
 }
